8cd70d115e9dc15816d0943169adec81926fa68f
HugoZedendahl <70585483+HugoZedendahl@users.noreply.github.com>
2025-09-15 20:43:56 +0200

    comment added

--- HugoZ övningar/dictionary not using dict.py
+++ HugoZ övningar/dictionary not using dict.py
@@ -1,13 +1,17 @@
 swedishStringList = ["hund", "katt", "ödla", "häst", "ko", "kanin"]
 englishStringList = ["dog", "cat", "lizzard", "horse", "cow", "rabbit"]
+indexValue = None
 
-indexValue = input("skriv ditt ord: ")
-indexValue = indexValue.lower()
-if indexValue in swedishStringList:
-    print("ditt ord på engelska är: ", englishStringList[swedishStringList.index(indexValue)])
+while indexValue != " ":
+    indexValue = input("skriv ditt ord: ")
+    indexValue = indexValue.lower()
+    if indexValue in swedishStringList:
+        print("ditt ord på engelska är: ", englishStringList[swedishStringList.index(indexValue)])
 
-elif indexValue in englishStringList:
-    print("ditt ord på svenska är: ", swedishStringList[englishStringList.index(indexValue)])
+    elif indexValue in englishStringList:
+        print("ditt ord på svenska är: ", swedishStringList[englishStringList.index(indexValue)])
 
-else:
-    print("ditt ord finns inte i vår ordbok")
+    else:
+        print("ditt ord finns inte i vår ordbok")
+
+#simple and effective. find the index of the input and use that to print the corresponding word from the other language. //HZ 
