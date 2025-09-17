c21e62f3418c7c7ca584c444e96b451c97b4ca8b
HugoZedendahl <70585483+HugoZedendahl@users.noreply.github.com>
2025-09-17 09:24:10 +0200

    läste fel på uppgiften, korrigerade funktionen.

--- HugoZ övningar/silly inventory thing.py
+++ HugoZ övningar/silly inventory thing.py
@@ -1,32 +1,53 @@
 gather = True
 mode = None
 printStatement = ""
-inventory = {
-    "körv": 200,
-    "brö": 400,
-    "elva": 1,
-    "höppa": 1,
-    }
+inventory = {}
+
 # you can also use 2 lists and and store the items and their amounts in the same index position. burt for this specific case
 # a dictionary is better (in my personal opinion) //HZ
-while gather:
-    mode = str.lower(input("what item(s) do you want to query? :"))
-    match mode:
-        case "körv":
-            if str(inventory["körv"])+" "+str(mode)+"ar" not in printStatement:
-                printStatement += str(inventory["körv"])+" "+str(mode)+"ar"+","
-        case "brö":
-            if str(inventory["brö"])+" "+str(mode)+"n" not in printStatement:
-                printStatement += str(inventory["brö"])+" "+str(mode)+"n"+","
-        case "elva":
-            if str(inventory["elva"])+" "+str(mode) not in printStatement:
-                printStatement += str(inventory["elva"])+" "+str(mode)+","
-        case "höppa":
-            gather = False
-        case _:
-            print("vi har int detta")
+#while gather:
+#    mode = str.lower(input("what item(s) do you want to query? :"))
+#    match mode:
+#        case "körv":
+#            if str(inventory["körv"])+" "+str(mode)+"ar" not in printStatement:
+#                printStatement += str(inventory["körv"])+" "+str(mode)+"ar"+","
+#        case "brö":
+#            if str(inventory["brö"])+" "+str(mode)+"n" not in printStatement:
+#                printStatement += str(inventory["brö"])+" "+str(mode)+"n"+","
+#        case "elva":
+#            if str(inventory["elva"])+" "+str(mode) not in printStatement:
+#                printStatement += str(inventory["elva"])+" "+str(mode)+","
+#        case "höppa":
+#            gather = False
+#        case _:
+#            print("vi har int detta")
+#
 
-print("vi ha:",printStatement)
+try:
+    inputArtickel = str(input("skriv in artickel: "))
+    inputArtickelLager = 0
+except: 
+    print("skriv en sträng! jag vet faktiskt inte hur du skrev in annan data")
+    quit()
+
+while inputArtickel != "sluta":
+    while inputArtickel != "sluta":
+        try: 
+            inputArtickelLager = int(input("skriv antal i lager: "))
+            if inputArtickelLager not in inventory:
+                inventory.update({inputArtickel:inputArtickelLager})
+            elif inputArtickelLager in inventory:
+                inventory[inputArtickel] = inputArtickelLager
+            inputArtickel = str(input("skriv in artickel: "))   
+        except:
+            print("snälla skriv en siffra")
+            break
+    
+
+print("vi har:")
+for key, value in inventory.items() :
+    print ("[",key,"]","[", value,"]")
+   
             
         
             
