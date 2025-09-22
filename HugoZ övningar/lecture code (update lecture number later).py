e70a635315eebab7ebc3de2ed20d650a59d8f5a7
HugoZedendahl <70585483+HugoZedendahl@users.noreply.github.com>
2025-09-22 09:48:57 +0200

    part 1

---
+++ HugoZ övningar/lecture code (update lecture number later).py
@@ -0,0 +1,23 @@
+def pause (message, rows, char):
+    for r in range(0, rows):
+        print(char*50)
+    input(message)
+
+print("javisst gör det ont när knoppar brister")
+pause("tryck på RETURN för andra raden",2,"-")
+print("varför skulle annars våren tveka")
+pause("tryck på RETURN för att avsluta",4,"+")
+
+pause("Hej", 4, "+")
+
+pause(char="-", rows=2, message="hej")
+#this thing non positional call  is uniqe to python
+
+# def pause (message, rows=3, char='-'):
+#    for r in range(0, rows):
+#        print(char*50)
+#    input(message)
+#   example of default values in functions. 
+#   these arguments need to be the last declared
+#   
+
